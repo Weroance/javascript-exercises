009225239584cde998414199b51ca5366a7a23e8
Weroance <danielrjansen@protonmail.com>
2024-04-23 16:45:51 -0400

    Add: javascript per instruction to pass jest tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(stringParam, repetitionParam) {
+    let stringConcat = ''
+    if (repetitionParam >= 0){
+        for (i = 0; i < repetitionParam; ++i){
+            stringConcat += stringParam;
+        }
+    }
+    else{
+        stringConcat = 'ERROR';
+        repetitionParam = 1   
+    }
+    return stringConcat
+}
 
 // Do not edit below this line
 module.exports = repeatString;
