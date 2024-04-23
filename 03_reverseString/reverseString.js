7113ec1f65a747106ff066680ac45fe6082b25d2
Weroance <danielrjansen@protonmail.com>
2024-04-23 17:27:18 -0400

    Add: code to pass jest tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringBefore) {
+const stringAfter = Array.from(stringBefore)
+    .reverse()
+    .join('')
+return stringAfter
 };
-
 // Do not edit below this line
 module.exports = reverseString;
