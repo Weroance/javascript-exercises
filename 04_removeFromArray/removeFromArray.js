ab6d389521610c9bbd7dc29aa48f83a4df0b4cbf
Weroance <danielrjansen@protonmail.com>
2024-04-24 12:50:28 -0400

    Add: code to satisfy jest tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(removeFromThese,...removeThese) {
 
+    //remover function to for loop over removeThese, is equal?
+    function remover(currentValue){
+        for(let i = 0; i < removeThese.length; i++){
+            
+          if ( currentValue === removeThese[i]){
+            return false;
+          }
+        }
+        return true
+    }
+    return removeFromThese.filter(remover)
 };
 
 // Do not edit below this line
