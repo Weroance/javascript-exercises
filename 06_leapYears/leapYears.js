f2d5c57d4b55d4815e46f2a9d4fa3321ca63a7f2
Weroance <danielrjansen@protonmail.com>
2024-04-25 10:01:45 -0400

    Add: code to satisfy jest test.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(y) {
+if( y % 400 == 0 )   return true
+if( y % 100 == 0 )   return false
+if( y % 4   == 0 )   return true
+
+return false
 
 };
 
