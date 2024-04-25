c284284d3723899aa995c3c37f659c43e50b33c8
Weroance <danielrjansen@protonmail.com>
2024-04-24 20:45:37 -0400

    Add:code to satisfy jest test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0
+    if (
+        ( a < 0 ) || 
+        ( b < 0 ) || 
+        ( typeof a !== 'number') ||
+        (typeof b !== 'number')){ 
+        return 'ERROR'
+    }
+    if ( a < b )   { 
+        low = a;
+        high = b 
+    }
+    else{
+         low = b; 
+         high = a;
+    }
+    while( low <= high ){
+        
+        sum += low;
+        low++;
+    }
+  
+    return sum;
 
 };
 
