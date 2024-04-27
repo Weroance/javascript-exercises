e0491e396e3a12a71d654e53dd137131922c59e8
Weroance <danielrjansen@protonmail.com>
2024-04-27 12:18:51 -0400

    Add: comments for rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
 const convertToCelsius = function(f) {
-return Math.round((( f - 32 ) / 1.8) * 10) / 10;
+// round to nearest tenths
+  return Math.round((( f - 32 ) / 1.8 ) * 10  ) / 10 ;
 };
 
 const convertToFahrenheit = function(c) {
-return Math.round((( c * 1.8 ) + 32 ) * 10) / 10;
+return Math.round((( c * 1.8 ) + 32 ) * 10  ) / 10 ;
 };
 
 // Do not edit below this line
