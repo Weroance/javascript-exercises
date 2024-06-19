3968165ce19eb2fce047c4eb749b5bfc33695897
Weroance <danielrjansen@protonmail.com>
2024-06-19 17:02:43 -0400

    Add: code to complete testx

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+ return a.reduce((accumulator,item) => {return accumulator += item}, 0 )
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  return a.reduce((accumulator,item) => {return accumulator *= item} )
+ };
 
+const power = function(a,b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arr = []
+	for (let i = a; i > 0; i--) {
+    arr.push(i)
+  }
+  return arr.reduce((accumulator,item) => { return accumulator *= item } , 1)
 };
 
 // Do not edit below this line
