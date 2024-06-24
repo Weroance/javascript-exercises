9b8f13f54310d0a6ef0c83d0a64b8a997db8c44a
Weroance <danielrjansen@protonmail.com>
2024-06-24 16:19:53 -0400

    Add: code to satisfy test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let number = Number(input)
+if(number > 0){  
+    let arr = [0,1];
+    let leng = 0
+    
+        for(let i = 1; i < number; i++ ){;
+            leng = arr.length - 1;
+            arr.push( arr[leng] + arr[leng - 1] );
 };
-
+    return arr[arr.length - 1];
+}
+else if( number < 0){return 'OOPS'}
+else{ return 0}
+}
 // Do not edit below this line
 module.exports = fibonacci;
